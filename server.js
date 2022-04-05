14ff32c22bb1e8a057401a81cd0caa0529b32946
Name4747 <684438@sylvaniastudents.org>
2022-04-05 13:11:24 -0400

    cosmos progress

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = new express()
+const dburl = 'https://beadsrhapsody-db.documents.azure.com:443/'
+const dbkey = 'XlUxEKsJ5b7ceJJ42dnmgX0RJsQrr7INkyRg2ZajdYCiwzcv1L3byQ8TF4IJANmtbT9XxO7n7nfUjdgIrJVWDA=='
 //const db = require('better-sqlite3')('orderform.db')
 const port = process.env.PORT || 8080
 const path = require('path')
@@ -12,10 +14,6 @@ app.post('/order', (req, res) => {
     //console.log(type,style,mat1,mat2,mat3,email,fullName,phone)
     const sql = db.prepare('INSERT INTO orders (type,style,material1,material2,material3,email,fullName,phone) VALUES (?,?,?,?,?,?,?,?)')
     const result = sql.run(type,style,mat1,mat2,mat3,email,fullName,phone)
-})
-
-app.get('/hello',(req,res) => {
-    res.send("Hallo, World!")
 })
 
 app.listen(port, () => {
